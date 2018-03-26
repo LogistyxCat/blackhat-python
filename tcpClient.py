655022b1246e7542b527a3c137f6e511defd1303
LogistyxCat <34379017+LogistyxCat@users.noreply.github.com>
2018-03-26 13:19:22 -0700

    Initial commit

---
+++ tcpClient.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+#Date: 2017-11-16
+#Description: Simple TCP client
+
+import socket
+target_host = "0.0.0.0"
+target_port = 9999
+
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect the client
+client.connect((target_host,target_port))
+
+#send some data
+client.send("ABCDEF\n")
+
+#receive some data
+response = client.recv(4096)
+
+print response
